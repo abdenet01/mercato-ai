12a7817a6320146bfabc6995a6956dff9d7195e3
abdenet <soulful376@gmail.com>
2026-07-18 03:50:17 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("Mercato AI JavaScript is working");
+async function loadProducts(){
+
+    try {
+
+        const response = await fetch(
+            "http://localhost:5000/products"
+        );
+
+
+        const products = await response.json();
+
+
+        console.log("Products from backend:");
+
+        console.log(products);
+
+
+    } catch(error){
+
+        console.log("Backend connection error:", error);
+
+    }
+
+}
+
+
+loadProducts();
